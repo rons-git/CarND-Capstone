7a940a444e2d9c183ee9b7950374e2d02afc63d5
pavelsimo <pavel.simo@gmail.com>
2019-04-13 14:37:25 +0200

    :zap: tuning throttle parameters

--- ros/src/twist_controller/twist_controller.py
+++ ros/src/twist_controller/twist_controller.py
@@ -16,14 +16,14 @@ class Controller(object):
         self.yaw_controller = YawController(wheel_base, steer_ratio, 0.1, max_lat_accel, max_steer_angle)
         kp = 0.3
         ki = 0.1
-        kd = 0.0
-        mn = 0.0   # minimum throttle value
-        mx = 0.2   # maximum throttle value
+        kd = 0
+        mn = 0.0    # minimum throttle value
+        mx = 0.3    # maximum throttle value
         self.throttle_controller = PID(kp, ki, kd, mn, mx)
         tau = 0.5  # cutoff frequency: 1 / 2pi*tau
         ts = 0.02  # sample time
         self.vel_lpf = LowPassFilter(tau, ts)
-        self.vehicle_mass = vehicle_mass
+        self.vehicle_mass = vehicle_mass + GAS_DENSITY*fuel_capacity
         self.fuel_capacity = fuel_capacity
         self.brake_deadband = brake_deadband
         self.decel_limit = decel_limit
@@ -56,12 +56,12 @@ class Controller(object):
         brake = 0
 
         if linear_vel == 0.0 and cur_vel < 0.1:
-            throttle = 0
+            throttle = 0.0
             # to prevent Carla from moving if we are stopped applying approximately 700 Nm of torque
-            brake = 700
+            brake = 700.0
 
         elif throttle < 0.1 and vel_error < 0:
-            throttle = 0
+            throttle = 0.0
             decel = max(vel_error, self.decel_limit)
             brake = abs(decel) * self.vehicle_mass * self.wheel_radius  # torque N*m
 
