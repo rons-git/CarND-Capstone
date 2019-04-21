7a71db032ef5b49ef217f0d4af96f1d86c5bf633
rons-git <ron@cancunron.com>
2019-04-21 15:31:23 -0400

    update from windows

--- scripts/init.bat
+++ scripts/init.bat
@@ -3,6 +3,6 @@ copy /y docker.sav docker.sh
 cscript dos2unix.vbs "docker.sh"
 cd ..
 docker build . -t capstone
-docker run -p 4567:4567 -v %cd%:/capstone -v /tmp/log:/root/.ros/ -e PYTHONUNBUFFERED=0 --rm -it capstone
-exit
+docker run -p 4567:4567 -v %cd%:/capstone -v /tmp/log:/root/.ros/ --rm -it capstone
+set /p=
 
