eb3e4541bd10535deac2f7cc5cd1b9e323b50b2c
rons-git <ron@cancunron.com>
2019-04-21 20:28:22 -0400

    update from windows

--- scripts/native.sh
+++ scripts/native.sh
@@ -1,10 +1,10 @@
 cd ..
 echo "Building the environment"
 catkin_make
-source /capstone/ros/devel/setup.bash
-cd /capstone/ros
-echo "Running roscore"
-roscore &
-sleep 5
-echo "Launching the nodes"
-roslaunch /capstone/ros/launch/styx.launch &
+#source /capstone/ros/devel/setup.bash
+#cd /capstone/ros
+#echo "Running roscore"
+#roscore &
+#sleep 5
+#echo "Launching the nodes"
+#roslaunch /capstone/ros/launch/styx.launch &
