07c54f7897fea8334bc8acd66bd7cd3107451b0b
rons-git <ron@cancunron.com>
2019-04-23 13:44:24 -0400

    update from windows

---
+++ scripts/docker.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+cd ..
+apt-get install dos2unix
+sleep 5
+find . -name '*.py' -exec dos2unix {} \;
+sleep 1
+find . -name '*.sh' -exec dos2unix {} \;
+sleep 1
+cd ros/src
+source /capstone/ros/devel/setup.bash
+cd /capstone/ros
+echo "Building the environment"
+catkin_make
+echo "Running roscore"
+roscore &
+sleep 5
+echo "Launching the nodes"
+roslaunch /capstone/ros/launch/styx.launch &
+read -p "Press enter to stop"
+sleep 5
+cd ..
+cd scripts
+rm d2utmp* 2> /dev/null
+read -p "Press enter to exit"
--- scripts/init-docker.bat
+++ scripts/init-docker.bat
@@ -4,4 +4,5 @@ cscript dos2unix.vbs "docker.sh"
 cd ..
 docker build . -t capstone
 docker run -p 4567:4567 -v %cd%:/capstone -v /tmp/log:/root/.ros/ --rm -it capstone
+set /=p
 
